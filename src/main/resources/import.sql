ccb06df99d91c8ca71540e0602e3b7b402ca1e0b
SalcedoEsteban <estebansalcedoalvarez@gmail.com>
2021-03-09 21:57:52 -0500

    se cambia el datasource a postresql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO funcionarios (id, nombre, apellido, cargo, fecha_creacion) VALUES (1, 'Esteban', 'Salcedo Alvarez', 'Guarda de Seguridad', '2021-03-06');
-INSERT INTO funcionarios (id, nombre, apellido, cargo, fecha_creacion) VALUES (2, 'Bernardo', 'Salcedo Alvarez', 'Jefe de Seguridad', '2021-03-06');
+INSERT INTO funcionarios (nombre, apellido, cargo, fecha_creacion) VALUES ('Esteban', 'Salcedo Alvarez', 'Guarda de Seguridad', '2021-03-06');
+INSERT INTO funcionarios (nombre, apellido, cargo, fecha_creacion) VALUES ('Bernardo', 'Salcedo Alvarez', 'Jefe de Seguridad', '2021-03-06');
